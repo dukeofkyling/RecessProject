fe55daf3f9e2bb646fc993b0bd77c4d232a233b6
dukeofkyling <freshlawrencejunior@gmail.com>
2024-07-09 12:42:59 +0300

    first commit all files
    I have committed the laravel files

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,
+        initial-scale=1">
+        <title> </title>
+</head>
+<body>
+    <h1>ADMIN</h1>
+    <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+
+                    <input type="submit" value="Logout">
+                </form>
+
+</body>
+</html>
