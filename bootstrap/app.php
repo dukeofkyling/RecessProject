ecdafd53876f2409e11224fa7d696f3aff67b37e
Tendo Caisey <tendocaisey16@gmail.com>
2024-07-15 21:25:25 +0300

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,11 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->([
-
-
-            'admin' => \App\Http\Middleware\Admin::class,
-        ]);
+       
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
