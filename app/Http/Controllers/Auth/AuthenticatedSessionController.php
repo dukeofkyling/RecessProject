a9527e763f9aecda0d88764dfba2507f388bcc3f
okelloemma01 <166294506+okelloemma01@users.noreply.github.com>
2024-07-16 15:00:08 +0300

    admin authentication

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        if($requesr->user()->usertype == 'admin')
+        if($requesr->user()->usertype === 'admin')
         {
             return redirect('admin/dashboard');
         }
